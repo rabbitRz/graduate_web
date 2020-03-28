d1d64f9f3f942f24daf26405e0ad7b4a6709ae55
yuanqi <2780085719@qq.com>
2020-03-29 00:08:24 +0800

    commit

--- src/com/bruip/hr/servlet/LoginServlet.java
+++ src/com/bruip/hr/servlet/LoginServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -33,8 +34,15 @@ public class LoginServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		//设置字符编码
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        //响应内容类型编码设置
+        response.setContentType("text/html;charset=UTF-8");
 		String username=request.getParameter("username");
 		String pwd=request.getParameter("password");
+		String rem=request.getParameter("remember");
+		System.out.print(rem);
 		LoginDao dao=new LoginDaoImpl();
 		boolean flag=false;
 		login_person lo=new login_person();
@@ -48,12 +56,33 @@ public class LoginServlet extends HttpServlet {
 				e.printStackTrace();
 			}
 		}
+		if("true".equals(rem)&&flag) {
+			Cookie ck1=new Cookie("remember",rem);
+			ck1.setMaxAge(60*6);
+			response.addCookie(ck1);
+			Cookie ck2=new Cookie("username",username);
+			ck2.setMaxAge(60*60);
+			response.addCookie(ck2);
+			Cookie ck3=new Cookie("password",pwd);
+			ck3.setMaxAge(60*60);
+			response.addCookie(ck3);
+		}else {
+			//清除cookie
+			Cookie[] cks=request.getCookies();
+			if(cks!=null) {
+				for(Cookie c:cks) {
+					c.setMaxAge(0);
+					response.addCookie(c);
+				}
+			}
+		}
 		if(flag) {
 			HttpSession session=request.getSession();
 			session.setAttribute("login_person", lo);
 			response.sendRedirect("NewFile.html");
 		}
 		else {
+			response.getWriter().write("登陆失败！");
 			response.sendRedirect("login.html");
 		}
 	}
